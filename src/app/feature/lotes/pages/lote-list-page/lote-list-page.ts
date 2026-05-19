7f73607a4dd14870861c0fbde794505fe7759c1c
Ednei Filho <ednei.goncalves@evoluum.com.br>
2026-05-19 10:13:25 -0300

    Estilização de cores para campo Status

--- src/app/feature/lotes/pages/lote-list-page/lote-list-page.ts
+++ src/app/feature/lotes/pages/lote-list-page/lote-list-page.ts
@@ -1,5 +1,9 @@
 import {Component, inject, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 import {LoteService} from '../../services/lote';
 import {Lote} from '../../models/lote.model';
 import {LoteStatus} from '../../models/lote-status.enum';
@@ -7,12 +11,24 @@ import {LoteStatus} from '../../models/lote-status.enum';
 @Component({
   selector: 'app-lote-list-page',
   imports: [
-    CommonModule
-  ],
-  templateUrl: './lote-list-page.html',
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
+  ],  templateUrl: './lote-list-page.html',
   styleUrl: './lote-list-page.css'
 })
 export class LoteListPage {
+
+  statusSelecionado = signal<LoteStatus | null>(null);
+
+  statusOptions = [
+    {label: 'Todos', value: null},
+    {label: 'Pendente', value: LoteStatus.PENDENTE},
+    {label: 'Exportado', value: LoteStatus.EXPORTADO},
+    {label: 'Rejeitado', value: LoteStatus.REJEITADO}
+  ];
 
   private loteService = inject(LoteService);
 
@@ -20,11 +36,15 @@ export class LoteListPage {
   loading = signal(false);
   error = signal<string | null>(null);
 
+  filtrar(): void {
+    this.carregar();
+  }
+
   carregar(): void {
     this.loading.set(true);
     this.error.set(null);
 
-    this.loteService.listar()
+    this.loteService.listar(this.statusSelecionado() ?? undefined)
       .subscribe({
         next: response => {
           this.lotes.set(response.content);
